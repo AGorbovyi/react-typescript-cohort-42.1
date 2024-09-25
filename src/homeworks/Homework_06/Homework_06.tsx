2a592465b9c5bd5853635860d37f5aa42145f87b
AGorbovyi <anton.gorbovyi@gmail.com>
2024-09-25 20:06:44 +0200

    homework 06

--- src/homeworks/Homework_06/Homework_06.tsx
+++ src/homeworks/Homework_06/Homework_06.tsx
@@ -1,41 +1,30 @@
-import "./styles.css"
+import { ReactNode } from "react";
+import { v4 } from "uuid";
+import { Car } from "./types";
+import "./styles.css";
 
 function Homework_06() {
+  const cars: Car[] = [
+    { brand: "BMW", price: 20000, isDiesel: true },
+    { brand: "Mercedes", price: 22000, isDiesel: false },
+    { brand: "Porshe", price: 50000, isDiesel: true },
+    { brand: "Nissan", price: 25000, isDiesel: false },
+    { brand: "Audi", price: 50000, isDiesel: true },
+  ];
 
-    interface Car {
-        brand: string,
-        price: number,
-        isDiesel: boolean
-    };
-    
-    const cars: Car[] = [ 
-        { brand: "BMW", price: 20000, isDiesel: true }, 
-        { brand: "Mercedes", price: 22000, isDiesel: false }, 
-        { brand: "Porshe", price: 50000, isDiesel: true }, 
-        { brand: "Nissan", price: 25000, isDiesel: false }, 
-        { brand: "Audi", price: 50000, isDiesel: true } 
-    ];
+  const listItems: ReactNode = cars.map((car: Car) => {
+    const cardId: string = v4();
+    return (
+      <div key={cardId} className="card-container">
+        <p className="card-title">Car information:</p>
+        <p className="card-info">Brand: {car.brand}</p>
+        <p className="card-info">Price: {car.price}</p>
+        <p className="card-info">Fuel: {car.isDiesel ? "Diesel" : "Petrol"}</p>
+      </div>
+    );
+  });
 
-    const listItems = cars.map((car) => {
-        return (
-            <div className='card-container'>
-                <p className='card-title'>Car information:</p>
-                <p className='card-info'>Brand: {car.brand}</p>
-                <p className='card-info'>Price: {car.price}</p>
-                <p className='card-info'>Diesel: {car.isDiesel}</p>
-            </div>
-        );
-      });      
-
-    
-return (
-    <div className="homework-06-wrapper">
-         {listItems}
-    </div>
-);
-
+  return <div className="homework-06-wrapper">{listItems}</div>;
 }
-
-
 
 export default Homework_06;
