695f269b59eee00068446f94395a656baafca69a
AGorbovyi <anton.gorbovyi@gmail.com>
2024-09-25 18:00:18 +0200

    homework 06

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 // HOMEWORK
+import Homework_06 from "homeworks/Homework_06/Homework_06";
 
 // LESSONS
 import Lesson_06 from "lessons/Lesson_06/Lesson_06";
@@ -11,7 +12,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Lesson_06 />
+      {/* <Lesson_06 /> */}
+      <Homework_06 />
     </div>
   );
 }
