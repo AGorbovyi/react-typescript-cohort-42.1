803cd08066da80ddca9d3d97ee4313552283e4c8
AGorbovyi <anton.gorbovyi@gmail.com>
2024-09-30 16:02:01 +0200

    Lesson 08

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-// HOMEWORK
+import GlobalStyles from "styles/GlobalStyles";
+
+// HOMEWORKS
 import Homework_06 from "homeworks/Homework_06/Homework_06";
+// import Homework_07 from "homeworks/Homework_07/Homework_07";
 
 // LESSONS
 import Lesson_06 from "lessons/Lesson_06/Lesson_06";
 import Lesson_07 from "lessons/Lesson_07/Lesson_07";
+import Lesson_08 from "lessons/Lesson_08/Lesson_08";
 
-
-// CONSULTATIONS
-
-import './App.css';
+//CONSULTATIONS
+// import Consultation_03 from "consultations/Consultation_03/Consultation_03";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <GlobalStyles />
       {/* <Lesson_06 /> */}
       {/* <Homework_06 /> */}
-      <Lesson_07 />
-    </div>
+      {/* <Lesson_07 /> */}
+      <Lesson_08 />
+    </>
   );
 }
 
